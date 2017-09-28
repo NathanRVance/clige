d8640c9302da9c4ba4f84380d9d55a95d9dcb2df
Nathan Vance <natervance@gmail.com>
2017-09-28 08:51:05 -0400

    Added rudimentary key inputs

--- clige.py
+++ clige.py
@@ -4,39 +4,64 @@ from window import Window
 from window import Cell
 from panel import Panel
 import logging
+import configparser
 
 # Various global variables for message passing
 wantsStop = False
+windowStack = []
+config = configparser.ConfigParser()
+config.read('config')
+keys = config['keys']
+for key in keys:
+    if keys[key] == 'TAB':
+        keys[key] = '\t'
+    # And so on for other special keys
 
-class Content():
-    def __init__(self):
+# initCallback(Cell) is called only once, and is intended for the initial setup.
+# refreshMethod(mainWindow) is called before every screen refresh
+def startCurses(initCallback, initContent, refreshMethod, keyPressCallback):
+    logging.basicConfig(filename=config['DEFAULT']['logfile'], format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    global windowStack
+    def main(cWin):
+        curses.curs_set(0)
+        cWin.clear()
+        cWin.nodelay(1)
+        windowStack.append(Window(cWin, Cell(Panel(initContent), None), False))
+        windowStack[0].rootCell.window = windowStack[0]
+        initCallback(windowStack[0].rootCell)
+        while not wantsStop:
+            procInput(windowStack[-1].cWin, keyPressCallback)
+            refreshMethod(windowStack[-1])
+            windowStack[-1].draw()
+            curses.doupdate()
+    from curses import wrapper
+    wrapper(main)
+
+# Process input
+def procInput(cWin, inputCallback):
+    try:
+        while True:
+            key = cWin.getkey()
+            if key == config['keys']['switch_focus']:
+                logging.debug('Tab key pressed')
+            else:
+                inputCallback(key)
+    except curses.error:
         pass
 
-    def getContent(self, width, height):
-        raise NotImplementedError()
+def width():
+    return windowStack[0].width()
 
-    # Returns true if this content should be continuously refreshed, false if it is static content (e.g., title bar)
-    def dynamic(self):
-        raise NotImplementedError()
+def height():
+    return windowStack[0].height()
 
-class Clige():
-    # refreshMethod(mainWindow) is called before every screen refresh
-    def __init__(self, refreshMethod):
-        self.refreshMethod = refreshMethod
-        logger = logging.basicConfig(filename='clige.log', format='%(levelname)s: %(message)s', level=logging.DEBUG)
-
-    # initCallback(Cell) is called only once, and is intended for the initial setup. Cell contains a dummy Panel, containing an unimplemented Content.
-    def startCurses(self, initCallback):
-        def main(cWin):
-            curses.curs_set(0)
-            self.cWin = cWin
-            cWin.clear()
-            self.mainWindow = Window(cWin, Cell(Panel(Content()), None), False)
-            self.mainWindow.rootCell.window = self.mainWindow
-            initCallback(self.mainWindow.rootCell)
-            while not wantsStop:
-                self.refreshMethod(self.mainWindow)
-                self.mainWindow.draw()
-                curses.doupdate()
-        from curses import wrapper
-        wrapper(main)
+def spawnWindow(winWidth, winHeight, content):
+    if winWidth > width():
+        winWidth = width()
+    if winHeight > height():
+        winHeight = height()
+    newCWin = curses.newwin(winHeight, winWidth, int((height() - winHeight) / 2), int((width() - winWidth) / 2))
+    newCWin.nodelay(1)
+    windowStack.append(Window(newCWin, Cell(Panel(content), None), True))
+    windowStack[-1].rootCell.window = windowStack[-1]
+    return windowStack[-1].rootCell
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import clige
 from panel import Panel
+import logging
 
 class mainContent():
     def __init__(self, contNum):
@@ -14,13 +15,18 @@ class mainContent():
         return self.contNum == 3
 
 def init(cell):
-    cell.panel.content = mainContent(1)
     [cell1, cell2] = cell.split(Panel(mainContent(2)), True, False)
     cell1.split(Panel(mainContent(3)), False, False)
 
-def refresh(mainWin):
-    pass
 
+hasRefreshed = False
+def refresh(topWindow):
+    global hasRefreshed
+    if not hasRefreshed:
+        hasRefreshed = True
+        clige.spawnWindow(30, 30, mainContent(4))
 
-clig = clige.Clige(refresh)
-clig.startCurses(init)
+def keyPress(key):
+    logging.debug('Key {} pressed'.format(key))
+
+clige.startCurses(init, mainContent(1), refresh, keyPress)
